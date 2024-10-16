6f5762098775d22be2ac46fb6a5904b1e629599a
CompuMath AI <gnohead@gmail.com>
2024-10-16 14:37:31 +0900

    begin simply

--- run.sh
+++ run.sh
@@ -3,12 +3,9 @@
 current_branch=$(git rev-parse --abbrev-ref HEAD)
 
 if [[ $current_branch == "dev" ]]; then
-    streamlit run ./app/app_irp_agent.py \
-        --server.address=172.21.0.2 
-
+    echo "development mode..."
 else
-    streamlit run ./app/app_irp_agent.py \
-        --server.address=172.21.0.2 
+    echo "service mode..."
 fi
 
 echo "done."
