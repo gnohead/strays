8e52b75e90fd1cda1421501d6f7f7610e0fe4044
gnohead <gnohead@gmail.com>
2024-10-25 08:45:51 +0900

    pyinstaller로 실행 파일시에도 대응하도록 ROOT 수정

--- app/framework/roots.py
+++ app/framework/roots.py
@@ -9,13 +9,20 @@
 import os, sys
 from pathlib import Path
 
-ROOT = lambda pt: Path(os.path.commonpath([__file__, sys.executable])).joinpath(pt).resolve()
+if getattr(sys, "frozen", False):
+    # PyInstaller로 패키징된 실행 파일의 경우
+    ROOT = lambda pt: Path(sys.executable).parent.joinpath(pt).resolve()
+else:
+    # 일반적인 스크립트 실행 환경인 경우
+    ROOT = lambda pt: Path(os.path.commonpath([__file__, sys.executable])).joinpath(pt).resolve()
+
 ROOT_FRAMEWORK = lambda pt: ROOT(__file__).parent.joinpath(pt).resolve()
 ROOT_APPDATA = lambda pt: ROOT("./__appdata__").joinpath(pt).resolve()
 ROOT_APPDATA(".").mkdir(parents=True, exist_ok=True)
 
 
 if __name__ == "__main__":
-    from __init__ import print
+    # from __init__ import print
+    print(ROOT("."))
 
     print("done.")
